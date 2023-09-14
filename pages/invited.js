5fb2e5f59647637d1e4105631a5dd8563564db5c
ericjiji3 <eric.ji3333@gmail.com>
2023-09-14 13:15:10 -0500

    wroking on emailjs

--- pages/invited.js
+++ pages/invited.js
@@ -1,14 +1,21 @@
 import styles from '@/styles/Invited.module.css'
-import {useState,  useEffect} from 'react';
+import {useState,  useEffect, useRef} from 'react';
 import macStyles from '@sakun/system.css';
 import Image from 'next/image';
 import im1 from '../public/images/dialogimg1.jpg';
 import im2 from '../public/images/dialogimg2.png';
 import im3 from '../public/images/dialogimg3.png';
+import emailjs from '@emailjs/browser';
 
 export default function Invited(){
     const [showDialog, setShowDialog] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [nameErr, setNameErr] = useState(false);
+    const [emailErr, setEmailErr] = useState(false);
 
+    const form = useRef();
+    
     useEffect(()=>{
         let setDialog = setTimeout(()=>{
             setShowDialog(true);
@@ -17,6 +24,66 @@ export default function Invited(){
             clearTimeout(setDialog);
         }
     })
+
+    function validateFields(data){
+        var validate = true;
+        Object.keys(data).forEach(function(field, index){
+            if(field == 'name'){
+                if(!data[field]){
+                    validate = false;
+                    setNameErr(true);
+                }
+            }
+            if(field == 'email'){
+                var emailData = data[field];
+                emailData.toLowerCase();
+                if(!emailData.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) || !email){
+                    validate = false;
+                    setEmailErr(true);
+                }
+            }
+        })
+        return validate;
+    }
+    function submitForm(e){
+        e.preventDefault();
+        setNameErr(false);
+        setEmailErr(false);
+        console.log("name", name)
+        console.log("email", email);
+        if(name == ''){
+            setNameErr(true);
+            console.log("name err");
+        }
+        if(!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) || !email){
+            setEmailErr(true);
+            console.log("email err");
+        }
+        var data = {
+            'name' : name,
+            'email' : email
+        }
+
+        if(validateFields(data)){
+            emailjs.sendForm('service_029dz8q', 'template_xtdv1r7', e.target, '3TfhFL9kwSBycbVYV')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+                e.target.reset();
+            });
+            console.log('success!!!!');
+        }else{
+            console.log('loser');
+        }
+
+        // if(!emailErr && !nameErr){
+
+            
+
+    }
+
     return(
         <div className={`${styles.invitedContainer} window`}>
             <div className={`title-bar`}>
@@ -32,13 +99,17 @@ export default function Invited(){
                         <div className={styles.textContainer}>
                         <h1 className={`${styles.header} dialog-text`}>Your Invited</h1>
                             <p className={`${styles.text} dialog-text`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus dictum at. Accumsan in nisl nisi scelerisque eu ultrices vitae. Porttitor eget dolor morbi non. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Euismod in pellentesque massa placerat duis ultricies lacus. Auctor neque vitae tempus quam pellentesque. Commodo elit at imperdiet dui accumsan sit amet nulla. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Arcu ac tortor dignissim convallis aenean et tortor at risus. In arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nulla facilisi morbi tempus iaculis.</p>
-                            <div className={styles.inputContainer}>
-                            <section className={`${styles.sectionContainer} field-row`} >
-                                <label for="text_find" className={`${styles.inputHeader} dialog-text`}>Email:</label>
-                                <input id="text_find" type="text" placeholder=""/>
-                                <button className={`btn`}>Cancel</button>
-                            </section>
-                            </div>
+                            <form className={styles.inputContainer} ref={form} onSubmit={submitForm}>
+                                <div className={styles.labelInput}>
+                                    <label for="name" className={`dialog-text`}>Full Name:</label>
+                                    <input id="name" name="name" type="text" placeholder="" value={name} onChange={(e) => setName(e.target.value)}/>
+                                </div>
+                                <div className={styles.labelInput}>
+                                    <label for="email" className={`dialog-text`}>Email:</label>
+                                    <input id="email" name="email" type="text" placeholder="" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                </div>
+                                <input className={`${styles.submitButt} btn`} type="submit" valaue="Submit"/>
+                            </form>
                         </div>
                         <div className={styles.imagesContainer}>
                             <div className={styles.imageContainer}>
